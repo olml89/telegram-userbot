78b9df992a8d0a835dad5f568d058620d6f5e34f
olml89 <oleguer.ml89@gmail.com>
2026-01-22 14:47:26 +0100

    feature/backend: Install symfony/web-profiler

--- backend/src/Infrastructure/Symfony/Kernel.php
+++ backend/src/Infrastructure/Symfony/Kernel.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace olml89\TelegramUserbot\Backend\Infrastructure\Symfony;
 
+use olml89\TelegramUserbot\Shared\App\Environment\Environment;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
@@ -21,16 +24,36 @@ final class Kernel extends BaseKernel
     public function registerBundles(): iterable
     {
         yield new FrameworkBundle();
+        yield new TwigBundle();
+
+        if ($this->getEnvironment() === Environment::Development->value) {
+            yield new WebProfilerBundle();
+        }
     }
 
     protected function configureContainer(ContainerConfigurator $container): void
     {
-        $container->import(resource: $this->getProjectDir() . '/config/framework.yaml');
+        /**
+         * Load framework configuration
+         */
+        $container->import(resource: $this->getConfigDir() . '/framework.yaml');
+
+        /**
+         * Load packages (Symfony related services)
+         * Overwrite with current environment packages configuration
+         */
+        $container->import(resource: $this->getConfigDir() . '/packages/**/*.yaml');
+        $container->import(resource: $this->getConfigDir() . '/packages/' . $this->getEnvironment() . '/**/*.yaml');
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import(resource: $this->getProjectDir() . '/config/routes.yaml');
+        /**
+         * Load routes
+         * Overwrite with current environment routes
+         */
+        $routes->import(resource: $this->getConfigDir() . '/routes/**/*.yaml');
+        $routes->import(resource: $this->getConfigDir() . '/routes/' . $this->getEnvironment() . '/**/*.yaml');
     }
 
     public function getProjectDir(): string
