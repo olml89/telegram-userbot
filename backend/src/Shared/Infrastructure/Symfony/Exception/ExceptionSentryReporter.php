bd710376adff726db91dfa5917f6f36b8cd0c38f
olml89 <oleguer.ml89@gmail.com>
2026-02-16 11:22:10 +0100

    TBOT-32: Refactor ExceptionSentryReporter to report successive aggregated exceptions after the main one

--- backend/src/Shared/Infrastructure/Symfony/Exception/ExceptionSentryReporter.php
+++ backend/src/Shared/Infrastructure/Symfony/Exception/ExceptionSentryReporter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace olml89\TelegramUserbot\Backend\Shared\Infrastructure\Symfony\Exception;
 
+use olml89\TelegramUserbot\Backend\Shared\Domain\Exception\ExceptionAggregator;
 use olml89\TelegramUserbot\Backend\Shared\Domain\Exception\Handler\ExceptionHandler;
 use Sentry\EventHint;
 use Sentry\ExceptionMechanism;
@@ -21,13 +22,26 @@ final readonly class ExceptionSentryReporter implements ExceptionHandler
 
     public function handle(Throwable $exception, bool $handled = true): void
     {
-        if ($this->isCritical($exception)) {
-            $hint = EventHint::fromArray([
-                'exception' => $exception,
-                'mechanism' => new ExceptionMechanism(ExceptionMechanism::TYPE_GENERIC, handled: $handled),
-            ]);
-
-            $this->sentry->captureException($exception, $hint);
+        if (!$this->isCritical($exception)) {
+            return;
         }
+
+        $this->reportException($exception, $handled);
+
+        if ($exception instanceof ExceptionAggregator) {
+            foreach ($exception->getAggregatedExceptions() as $aggregateException) {
+                $this->reportException($aggregateException, $handled);
+            }
+        }
+    }
+
+    private function reportException(Throwable $exception, bool $handled): void
+    {
+        $hint = EventHint::fromArray([
+            'exception' => $exception,
+            'mechanism' => new ExceptionMechanism(ExceptionMechanism::TYPE_GENERIC, handled: $handled),
+        ]);
+
+        $this->sentry->captureException($exception, $hint);
     }
 }
