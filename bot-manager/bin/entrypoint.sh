27f83a56f2ca1c1783fd4f45b602af0825d1f9d4
olml89 <oleguer.ml89@gmail.com>
2026-04-16 21:09:24 +0200

    TBOT-46: Refactor bot, bot-manager and backend entrypoints to only install dependencies if not on prod environment since then they are not backed into the container image

--- bot-manager/bin/entrypoint.sh
+++ bot-manager/bin/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
 
-# Install bot-manager dependencies
-/telegram-userbot/shared/bin/composer-install.sh shared bot-manager
+# In environments that are not prod, install dependencies (since they are not baked into the container image)
+if [ "$APP_ENV" != "prod" ]; then
+	/telegram-userbot/shared/bin/composer-install.sh shared bot-manager
+fi
 
 # Start bot-manager.php, which starts a React loop
 echo "✅ Container up [bin/bot-manager.php]."
--- bot/bin/entrypoint.sh
+++ bot/bin/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+# In environments that are not prod, install dependencies (since they are not baked into the container image)
+if [ "$APP_ENV" != "prod" ]; then
+    /telegram-userbot/shared/bin/composer-install.sh shared bot
+fi
+
 VAR="/telegram-userbot/bot/var"
 echo "🔧 Creating ${VAR} directory for the supervisord.pid file..."
 mkdir -p ${VAR}
@@ -8,9 +13,6 @@ mkdir -p ${VAR}
 LOG="/var/log/bot"
 echo "🔧 Creating ${LOG} directory for the supervisord.log in the shared container..."
 mkdir -p ${LOG}
-
-# Install bot dependencies
-/telegram-userbot/shared/bin/composer-install.sh shared bot
 
 # Use exec to replace the shell and start with PID 1
 CONF="/etc/supervisor/supervisord.conf"
