540e53af935121f35121e006d665ce4561ba0dca
olml89 <oleguer.ml89@gmail.com>
2026-05-18 22:09:08 +0200

    TBOT-51: Implement a recipe to reset the application to a factory state - Implement a dev/bin/setup/reset.sh to delete composer.lock, package-lock.json, node_modules, var and vendor from services and use it in a recipe after make down - Refactor dev/bin/setup/setup.sh to mkdir -p vite/var and vite/var/npm (and dev/var, that was missing) on non-prod environments - Separate completely npm from backend container, install it only on backend container (and on backend/Dockerfile assets stage, only to pull the compiled assets to the prod container without npm), not on backend/Dockerfile dev stage anymore - Use NPM_CONFIG_CACHE on vite container to avoid /.npm cache directory issues - npm install --include=dev on vite/entrypoint.sh, npm ci on backend/Dockerfile on assets stage - Upgrade backend/package-lock.json

--- dev/bin/setup/setup.sh
+++ dev/bin/setup/setup.sh
@@ -3,16 +3,15 @@ set -eu
 
 echo "🔧 Initializing runtime directories..."
 
-# Backend file managing directories
 if [ "$APP_ENV" != "prod" ]; then
 	mkdir -p .runtime/uploads
 	mkdir -p .runtime/content
+	mkdir -p dev/var
+	mkdir -p vite/var vite/var/npm
 fi
 
-# var directories
 mkdir -p application/var
-mkdir -p backend/var
-mkdir -p backend/var/cache
+mkdir -p backend/var backend/var/cache
 mkdir -p bot/var
 mkdir -p bot-manager/var
 mkdir -p bot-runtime/var
