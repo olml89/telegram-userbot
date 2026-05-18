613eafdaf4dbd391799fb592d35e9efaaf169a48
olml89 <oleguer.ml89@gmail.com>
2026-05-18 12:32:14 +0200

    TBOT-50: Change the order of dependencies installation on dev container

--- dev/bin/entrypoint.sh
+++ dev/bin/entrypoint.sh
@@ -5,8 +5,8 @@ echo "🔧 Loading pre-commit git hook..."
 cp bin/git/hooks/pre-commit /telegram-userbot/.git/hooks/pre-commit
 
 # Install dev dependencies
-/telegram-userbot/dev/bin/composer/composer-install.sh dev bot-runtime bot bot-manager backend
+/telegram-userbot/dev/bin/composer/composer-install.sh bot-runtime bot bot-manager backend dev
 
 # Maintain the container always up with tail
-echo "✅ Container up [tail -f /dev/null]."
+echo "✅ Container up [dev]."
 tail -f /dev/null
