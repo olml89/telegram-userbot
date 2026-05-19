9eb15bfab26cfa6f622eca5bce6705c68c81d5a1
olml89 <oleguer.ml89@gmail.com>
2026-05-19 18:29:07 +0200

    TBOT-52: Copy the compiled assets directly from assets step to /telegram-userbot/backend/public/build to avoid assets permission issues when copying from the intermediate directory

--- backend/bin/entrypoint.sh
+++ backend/bin/entrypoint.sh
@@ -1,23 +1,8 @@
 #!/bin/sh
 set -eu
 
-# In production environment, copy built assets from the intermediate directory to the public build
-if [ "$APP_ENV" = "prod" ]; then
-	COMPILED="/telegram-userbot/backend/var/build"
-	BUILD="/telegram-userbot/backend/public/build"
-	echo "🔧 Copying compiled assets from ${COMPILED} to ${BUILD}..."
-
-	if [ -d "${COMPILED}" ]; then
-		# Remove contents of BUILD, not BUILD itself
-		rm -rf "${BUILD:?}/"*
-		mkdir -p "${BUILD}"
-
-		# Copy the contents of COMPILED, not COMPILED itself
-		cp -R "${COMPILED}/." "${BUILD}"
-		rm -rf "${COMPILED}"
-	fi
-else
-	# In environments that are not prod, install dependencies (since they are not baked into the container image)
+# In environments that are not prod, install dependencies (since they are not baked into the container image)
+if [ "$APP_ENV" != "prod" ]; then
     /telegram-userbot/dev/bin/composer/composer-install.sh application backend
 fi
 
